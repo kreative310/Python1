6d139f98bd917f40eef21722e112668e9bc6520f
Studenti <kreative310k@gmail.com>
2024-11-02 11:47:36 +0100

    ...

--- python2.py
+++ python2.py
@@ -1,1 +1,16 @@
-print("hello")
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits)
+
+#create a tuple(doesnt change)
+words = ("spam", "eggs", "susages")
+print(words[1])
+
+#create a tuple with info about a person
+
+person = ('Keti', "18", "software engineer")
+
+name, age, proffesion = person
+
+print(name, "'s", "proffesion is", proffesion, "and she is", age, "years old.")
+
