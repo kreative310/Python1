1cc910193d6f0f78c18bfab54f5f693716ae5423
Studenti <kreative310k@gmail.com>
2024-11-02 12:04:34 +0100

    ...

--- python2.py
+++ python2.py
@@ -14,3 +14,41 @@ name, age, proffesion = person
 
 print(name, "'s", "proffesion is", proffesion, "and she is", age, "years old.")
 
+
+#creating dictionary
+
+my_dictionary = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+
+    #more keys more values pairs
+
+}
+
+contact_info = {
+    "Blerta": "7893-993",
+    "drini": "2764765489"
+}
+
+#create a variable called kreatives phone and use [] we can specify which key we want to access
+
+kreative_phone = contact_info ["Blerta"]
+
+print(kreative_phone)
+print(contact_info)
+
+contact_info ["Drini"] = "0294-48575"
+
+print(contact_info)
+
+#we want to add another friend to contact_info
+
+contact_info ["Eros"] = "9843-117348"
+
+print(contact_info)
+
+#delete a contact info
+
+del contact_info ["Blerta"]
+print(contact_info)
