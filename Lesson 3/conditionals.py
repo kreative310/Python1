b7d0770d888e1b5dc53e3870cbe2fbe4eddda6cb
Studenti <kreative310k@gmail.com>
2024-11-09 12:48:24 +0100

    ...

---
+++ Lesson 3/conditionals.py
@@ -0,0 +1,15 @@
+#conditionals
+
+age = 18
+
+if age => 18:
+    print("you can vote")
+else:
+    print("you cant vote"
+
+temp = 28
+
+if temp => 18:
+    print("its hot")
+else 20 <= temp <=30 :
+    print("its cold")
