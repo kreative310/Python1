be62471ed44a892cebb1e194263dc400fe06b896
Student <kreative310k@gmail.com>
2024-12-21 03:47:51 -0800

    ...

---
+++ lesson 7/oop.py
@@ -0,0 +1,46 @@
+def calcuate_area (length, width):
+    return length * width
+
+def calculate_perimeter (length, width):
+    return 2 * (length + width)
+
+length = 4
+width = 3
+
+area = calculate_area(length, width)
+perimeter = calculate_perimeter(length, width)
+
+print (f"Area: {area}")
+print (f"Parameter: {parameter}")
+
+class Rectangle:
+    def __init__ (self, length, width)
+        self.length = length
+        self.width = width
+
+    def calculate_area (length)
+        return self.length * self.width
+
+    def calculate_perimeter(width)
+        return 2 * (self.length + self.width)
+
+my rectangle = Rectangle (6, 4)
+area = my_rectangle.calculate_area()
+perimeter = my_rectangle.calculate_perimeter()
+
+print (f"Area: {area}")
+print (f"Perimeter: {perimeter}")
+
+class Person:
+    def __init__ (self, name, person):
+        self.name = name
+        self.age = age
+
+def greet(self):
+    print(f"hello I am {self.name} and i am {self.age} years old")
+
+person1 = Person ("kreative", "18")
+person2 = Person ("kiki", "22")
+
+person1.greet()
+
